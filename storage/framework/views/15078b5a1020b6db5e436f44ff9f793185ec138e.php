953bfdf793495e9e2d7eac756e030195b5a9717b
Farahat-Group <107654339+Farahat-Group@users.noreply.github.com>
2022-07-01 20:01:33 +0200

    Add files via upload

---
+++ storage/framework/views/15078b5a1020b6db5e436f44ff9f793185ec138e.php
@@ -0,0 +1,19 @@
+
+<br/>
+Dear <?php echo e($email); ?>
+
+<br/>
+<br>
+Your Password Change Request Is Successfully Done
+<br/><br/>
+<br/><br/>
+---------- <h2>Password</h2> ----------
+<br>
+
+Your New Password is : <br>
+
+<span style="color: red">
+    <?php echo e($password); ?>
+
+</span>
+<?php /**PATH C:\xampp\htdocs\eazly\resources\views/mailtemplate.blade.php ENDPATH**/ ?>
